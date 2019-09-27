fc61ffdbd63a09d6ee65fea70126eca66649337b
python240419 <51232248+python240419@users.noreply.github.com>
2019-09-27 11:48:44 +0300

    Create int_var.py

---
+++ int_var.py
@@ -0,0 +1,40 @@
+from tkinter import *
+
+# place items on self (window)
+# IntVar
+# StringVar
+# grid
+# validate?
+# listbox ?
+
+class CalculatorWindow:
+    def __init__(self, root):
+        self.root= root
+        root.title("Calculator")
+        # like this will not be accessed
+        # after this method
+        #total_label = Label(root, text="hello")
+        #total_label.pack()
+        # ----------------------------
+        # IntVar / StringVar
+        self.total_value = IntVar()
+        self.total_value.set(0)
+        self.total_label = Label(root,
+                                 textvariable=self.total_value,
+                                 font=("david", 72))
+        self.total_label.pack()
+        self.addBtn = Button(text="Add",
+                             #command=self.add1)
+                             command=lambda: self.total_value.set(self.total_value.get() + 1))
+        self.addBtn.pack()
+    def add1(self):
+        self.total_value.set(self.total_value.get() + 1)
+
+root = Tk()
+my_window = CalculatorWindow(root)
+root.mainloop()  # blocking
+
+# targil;
+# add button which decrease 1
+# add button which sets the value back to 0
+# with lambda?
