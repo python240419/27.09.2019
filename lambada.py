8a521652f440cceec82c842b90e63cc8b95d8c1c
python240419 <51232248+python240419@users.noreply.github.com>
2019-09-27 10:54:25 +0300

    Update lambada.py

--- lambada.py
+++ lambada.py
@@ -47,7 +47,7 @@ print(p2)
 #    get only positive numbers
 # 2. create map using lamda to
 #   get 1/3 of each number
-# [(1,2,3), (2,3), (1,2,3,4,5), (1)]
+# [(1,2,3), (2,3), (1,2,3,4,5), (1,)]
 # sort this list by the length of each tuple:
-# result = [(1), (2,3), (1,2,3), (1,2,3,4,5)]
+# result = [(1,), (2,3), (1,2,3), (1,2,3,4,5)]
 
